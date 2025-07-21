c1c8634cee6ac3b292721ab9e1c299ecabc495bd
priyadharshika28 <140396562+priyadharshika28@users.noreply.github.com>
2025-07-21 21:44:59 +0530

    Create longest word.java

---
+++ longest word.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String[] words = s.split(" ");
+        String longest = "";
+        for(String word:words){
+            if(word.length()>longest.length()){
+                longest = word;
+            }
+        }
+        System.out.println(longest);
+    }
+}
